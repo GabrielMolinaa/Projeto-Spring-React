b938dd55c8448db4046599eef76fbb14e2ceecf0
Gabriel Molina <gabrielmolinadelima@hotmail.com>
2022-07-11 17:38:21 -0300

    DatePicker funcionando

--- frontend/src/Components/SalesCard/index.tsx
+++ frontend/src/Components/SalesCard/index.tsx
@@ -2,24 +2,34 @@ import NotificationButton from '../notification-button'
 import './styles.css'
 import DatePicker from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
+import { useState } from 'react';
+
 
 function SalesCard() {
+
+    const min = new Date(new Date().setDate(new Date().getDate() - 365));
+    const max = new Date();
+
+    const [minDate,setMinDate] = useState(min);
+    const [maxDate, setMaxDate] = useState(max);
+
+
     return (
         <div className="DSMeta-card">
             <h2 className="DSMeta-sales-title">Vendas</h2>
             <div>
                 <div className="DSMeta-form-container">
                     <DatePicker
-                        selected={new Date()}
-                        onChange={(date: Date) => { }}
+                        selected={minDate}
+                        onChange={(date: Date) => setMinDate(date)}
                         className="DSMeta-form-control"
                         dateFormat="dd/MM/yyyy"
                     />
                 </div>
                 <div className="DSMeta-form-container">
                     <DatePicker
-                        selected={new Date()}
-                        onChange={(date: Date) => { }}
+                        selected={maxDate}
+                        onChange={(date: Date) => setMaxDate(date)}
                         className="DSMeta-form-control"
                         dateFormat="dd/MM/yyyy"
                     />
