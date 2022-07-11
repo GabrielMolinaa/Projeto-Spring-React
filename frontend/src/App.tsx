1a4e3a8145dbf77972593f99532a49530304588a
Gabriel Molina <gabrielmolinadelima@hotmail.com>
2022-07-11 15:54:25 -0300

    primeiro componente

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,12 @@
+import NotificationButton from "./Components/notification-button"
 
 function App() {
   return (
-    <h1>Olá Mundo</h1>
-    )
+    <>
+      <h1>Olá Mundo</h1>
+      <NotificationButton />
+    </>
+  )
 }
 
 export default App
---
+++ frontend/src/Components/notification-button/index.tsx
@@ -0,0 +1,14 @@
+import icon from '../../assets/img/notification-icon.svg'
+import './style.css'
+
+function NotificationButton() {
+    return (
+        
+        <div className="DSMeta-redButton">
+            <img src={icon} alt="notify"/>
+        </div>
+        
+    )
+}
+
+export default NotificationButton
