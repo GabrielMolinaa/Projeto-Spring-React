d1377cab079c8919301e12d0bb2426519c586d15
Gabriel Molina <gabrielmolinadelima@hotmail.com>
2022-07-18 13:19:56 -0300

    Sms Correct

--- backend/src/main/java/com/devsuperior/dsmeta/services/SmsService.java
+++ backend/src/main/java/com/devsuperior/dsmeta/services/SmsService.java
@@ -33,7 +33,7 @@ public class SmsService {
 		Sale sale = saleRepository.findById(saleId).get();
 		String date = sale.getDate().getMonthValue() + "/" + sale.getDate().getYear();
 		
-		String msg = "O Vendedor " + sale.getSellerName() + "foi destaque em" + date +
+		String msg = "O Vendedor " + sale.getSellerName() + " foi destaque em " + date +
 				" com um total de R$ "+ String.format("%.2f", sale.getAmout());
 		
 		
